6ed8b3116f4ce8d75d9cc1d64dea2547ba46c667
NathanDoile <doile.souza@gmail.com>
2024-09-25 22:48:57 -0300

    Base inicial API

---
+++ api/Gaxtos/src/main/java/com/gaxtos/Gaxtos/GaxtosApplication.java
@@ -0,0 +1,13 @@
+package com.gaxtos.Gaxtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaxtosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaxtosApplication.class, args);
+	}
+
+}
---
+++ api/Gaxtos/src/test/java/com/gaxtos/Gaxtos/GaxtosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaxtos.Gaxtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaxtosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
